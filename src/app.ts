04bc69456f857ac69784e683f5510350ffed2aad
rahul4019 <rahulkashyap4019@gmail.com>
2024-09-29 14:26:42 +0530

    Add api response type

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.status(200).json({
     success: true,
     msg: "Hello from chatlink-api",
   });
 });
 
-app.get("/test-db", async (req, res) => {
+app.get("/test-db", async (_, res) => {
   try {
     const result = await query("SELECT NOW()");
     return res.status(200).json({
