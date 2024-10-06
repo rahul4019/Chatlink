e450d245dba436a6a4138a9459ed7f6a95fc5893
rahul4019 <rahulkashyap4019@gmail.com>
2024-10-06 17:43:01 +0530

    Add multer middleware

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,15 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { ApiResponse } from "../types/apiResponse";
 
-export const updateProfilePicture = async (req: Request, res: Response) => {
+export const updateProfilePicture = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<Response<ApiResponse> | void> => {
   try {
-  } catch (error) {}
+
+  } catch (error) {
+    console.log("Error while updating profile picture: ", error);
+    next(error);
+  }
 };
---
+++ src/middleware/multer.ts
@@ -0,0 +1,39 @@
+import multer from "multer";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
+import CustomError from "../utils/customError";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, uuidv4() + ext);
+  },
+});
+
+const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback,
+) => {
+  const allowedTypes = /jpeg|jpg|png/;
+
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase(),
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new CustomError("Only jpeg, jpg and png images are allowed", 500));
+  }
+};
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: 1 * 1024 * 1024 }, // limit image size to 1MB
+  fileFilter,
+});
