b6943450435923bbe2729ad55f672bd74649c9c1
rahul4019 <rahulkashyap4019@gmail.com>
2024-10-07 07:25:45 +0530

    Error handling in s3Upload.ts

--- src/utils/s3Upload.ts
+++ src/utils/s3Upload.ts
@@ -1,6 +1,7 @@
 import AWS from "aws-sdk";
 import fs from "fs";
 import { promisify } from "util";
+import CustomError from "../utils/customError"; // Ensure you have a custom error handling class
 
 // aws sdk configuration
 const s3 = new AWS.S3({
@@ -11,31 +12,41 @@ const s3 = new AWS.S3({
 
 // upload the img to s3 bucket
 export const s3Upload = async (file: Express.Multer.File) => {
-  const fileContent = await promisify(fs.readFile)(file.path);
+  try {
+    const fileContent = await promisify(fs.readFile)(file.path);
 
-  const uploadParams = {
-    Bucket: process.env.AWS_S3_BUCKET_NAME as string,
-    Key: `profile_pictures/${file.filename}`, // images will be saved in profile_pictures directory
-    Body: fileContent,
-    ContentType: file.mimetype,
-  };
+    const uploadParams = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME as string,
+      Key: `profile_pictures/${file.filename}`, // Images will be saved in profile_pictures directory
+      Body: fileContent,
+      ContentType: file.mimetype,
+    };
 
-  // Uploading files to the bucket
-  const { Location } = await s3.upload(uploadParams).promise();
+    // Uploading files to the bucket
+    const { Location } = await s3.upload(uploadParams).promise();
 
-  // Delete the file from the local filesystem
-  fs.unlinkSync(file.path);
+    // Delete the file from the local filesystem
+    fs.unlinkSync(file.path);
 
-  return Location; // This is the file URL on S3
+    return Location;
+  } catch (error) {
+    console.log("Error uploading profile picture", error);
+    throw new CustomError("Could not upload profile picture", 500);
+  }
 };
 
 export const deleteExistingProfilePicture = async (url: string) => {
-  const fileName = url.split("/").pop();
+  try {
+    const fileName = url.split("/").pop();
 
-  const deleteParams = {
-    Bucket: process.env.AWS_S3_BUCKET_NAME as string,
-    Key: `profile_pictures/${fileName}`, // images will be saved in profile_pictures directory
-  };
+    const deleteParams = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME as string,
+      Key: `profile_pictures/${fileName}`,
+    };
 
-  await s3.deleteObject(deleteParams).promise();
+    await s3.deleteObject(deleteParams).promise();
+  } catch (error) {
+    console.log("Error deleting existing profile picture", error);
+    throw new CustomError("Could not delete existing profile picture", 500);
+  }
 };
