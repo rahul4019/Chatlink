46fc50b3e6dcd0bda2d77326fadfd62f156af662
rahul4019 <rahulkashyap4019@gmail.com>
2024-10-12 13:27:18 +0530

    Client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+    <>
+      <div className="font-semibold">Hello World!</div>
+      <Button> Click me</Button>
+    </>
+  );
+}
+
+export default App;
