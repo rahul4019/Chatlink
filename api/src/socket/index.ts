59fe353822073e132596f536d13ea86acda23553
rahul4019 <rahulkashyap4019@gmail.com>
2024-11-06 20:26:21 +0530

    Upgarad the onlineUsers object to manage multiple connection from same user

--- api/src/socket/index.ts
+++ api/src/socket/index.ts
@@ -5,7 +5,7 @@ import { chatHandler } from "./chatHandler";
 let io: Server;
 
 // in memory store to keep the track of online users
-const onlineUsers: {[userId: string]:string} = {} // here userId is dynamic key
+const onlineUsers: { [userId: string]: string[] } = {}; // here userId is dynamic key
 
 export const initSocket = (httpServer: HttpServer) => {
   io = new Server(httpServer, {
@@ -18,13 +18,21 @@ export const initSocket = (httpServer: HttpServer) => {
   const sendMessage = chatHandler(io);
 
   io.on("connection", (socket) => {
-    // retriving userId from the handshake 
-    const userId = socket.handshake.auth.userId
-    
-    onlineUsers[userId] = socket.id
+    // retriving userId from the handshake
+    const userId = socket.handshake.auth.userId;
 
-    console.log(onlineUsers)
-    
+    // onlineUsers[userId] = socket.id
+
+    if (userId) {
+      if (onlineUsers[userId]) {
+        onlineUsers[userId].push(socket.id);
+      } else {
+        onlineUsers[userId] = [socket.id];
+      }
+    }
+
+    console.log(`userId: ${userId} connected with socket ID: ${socket.id}`);
+
     // console.log("client connected");
     // socket.on("chat:sendMessage", (message) => {
     //   console.log("received message: ", message);
