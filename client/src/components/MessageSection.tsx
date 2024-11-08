db791479bf3fb6f48fca919975b4997d4ca33dcf
rahul4019 <rahulkashyap4019@gmail.com>
2024-11-08 13:02:41 +0530

    Add scrolling on for new messages

--- client/src/components/MessageSection.tsx
+++ client/src/components/MessageSection.tsx
@@ -2,11 +2,22 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
 import { Skeleton } from "./ui/skeleton";
 import { useAppSelector } from "@/app/hooks";
-import {Message} from '@/types/chat'
+import { Message } from "@/types/chat";
+import { useEffect, useRef } from "react";
 
 const MessageSection = () => {
+  const chatEndRef = useRef<HTMLDivElement | null>(null);
   const { user } = useAppSelector((state) => state.auth);
   const { chats, loading } = useAppSelector((state) => state.chat);
+
+  const scrollToBottom = () => {
+    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // scrolls whenever there is change in the chats
+  useEffect(() => {
+    scrollToBottom();
+  }, [chats]);
 
   return (
     <ScrollArea className="flex-1 p-4">
@@ -26,39 +37,29 @@ const MessageSection = () => {
               </div>
             </div>
           ))
-        : chats.map(
-            (
-              message: Message,
-              index: number,
-            ) => (
-              <motion.div
-                key={message.id}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: index * 0.1 }}
-                className={`flex ${message.sender_id === user?.id ? "justify-end" : "justify-start"} mb-4`}
+        : chats.map((message: Message) => (
+            <div
+              key={message.id}
+              className={`flex ${message.sender_id === user?.id ? "justify-end" : "justify-start"} mb-4`}
+            >
+              <div
+                className={`max-w-[70%] px-3 pt-2 pb-1 ${
+                  message.sender_id === user?.id
+                    ? "bg-primary text-primary-foreground rounded-tl-2xl rounded-bl-2xl rounded-br-2xl"
+                    : "bg-secondary text-secondary-foreground rounded-tr-2xl rounded-br-2xl rounded-bl-2xl"
+                }`}
               >
-                <motion.div
-                  initial={{ scale: 0.8 }}
-                  animate={{ scale: 1 }}
-                  transition={{ type: "spring", stiffness: 500, damping: 30 }}
-                  className={`max-w-[70%] px-3 pt-2 pb-1 ${
-                    message.sender_id === user?.id
-                      ? "bg-primary text-primary-foreground rounded-tl-2xl rounded-bl-2xl rounded-br-2xl"
-                      : "bg-secondary text-secondary-foreground rounded-tr-2xl rounded-br-2xl rounded-bl-2xl"
-                  }`}
-                >
-                  <p>{message.message_text}</p>
-                  <p className="text-xs text-end mt-2 opacity-70">
-                    {new Date(message.sent_at).toLocaleString("en-us", {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}
-                  </p>
-                </motion.div>
-              </motion.div>
-            ),
-          )}
+                <p>{message.message_text}</p>
+                <p className="text-xs text-end mt-2 opacity-70">
+                  {new Date(message.sent_at).toLocaleString("en-us", {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </p>
+              </div>
+            </div>
+          ))}
+      <div ref={chatEndRef} />
     </ScrollArea>
   );
 };
